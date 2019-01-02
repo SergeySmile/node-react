e792ae4ad4db07480f23f815b06e6d54f5f01c7c
smile <sergey.smille@gmail.com>
2019-01-02 19:43:21 +0200

    redux thunk, action creaters, api proxy, css materialize

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
+import Header from './Header';
+
+const DashBoard = () => {
   return (
-   <div>
-     Hi there!
-   </div>
+    <div>
+      <h2>DashBoard</h2>
+    </div>
   );
 };
 
-export default App;
+const SurveyNew = () => {
+  return (
+    <div>
+      <h2>SurveyNew</h2>
+    </div>
+  );
+};
+
+const Landing = () => {
+  return (
+    <div>
+      <h2>Landing</h2>
+    </div>
+  );
+};
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={DashBoard} />
+            <Route path="/survey/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
